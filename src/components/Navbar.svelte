c694dd38eb29faf354a14a56dc2103ea6d352e11
RaedRNP <raedrnp10@gmail.com>
2024-12-15 01:09:38 -0400

    Inhabilitando dos enlaces que estaran en desuso

--- src/components/Navbar.svelte
+++ src/components/Navbar.svelte
@@ -20,13 +20,13 @@
     <span class="self-center whitespace-nowrap text-xl font-semibold dark:text-white">{SiteName}</span>
   </NavBrand>
   <div class="flex items-center md:order-2">
-    <Button class='mr-10 -ml-20 hidden md:flex uppercase bg-[#0766AD] hover:bg-[#1097ff]' onclick={()=>goto('/inicio-sesion')}>
+    <Button class='mr-10 -ml-20 hidden md:flex uppercase bg-[#0766AD] hover:bg-[#1097ff]' onclick={()=>goto('/user')}>
       Jugar <ArrowRightOutline class="w-5 h-5 ms-2" />
     </Button>
     {#if User}
       <Avatar id="avatar-menu" src="/favicon.png" class='cursor-pointer' />
     {:else}
-      <a href="/inicio-sesion">Iniciar Sesión</a>
+      <a href="/user">Iniciar Sesión</a>
     {/if}
     <NavHamburger class1="w-full md:flex md:w-auto md:order-1" />
   </div>
@@ -43,8 +43,8 @@
   <NavUl class='md:absolute md:left-1/2 md:-translate-x-1/2'>
     <NavLi class='hover:text-[#1097ff]' href="/" active={true}>Inicio</NavLi>
     <NavLi class='hover:text-[#1097ff]' href="/soporte">Soporte</NavLi>
-    <NavLi class='hover:text-[#1097ff]' href="/blog">Blog</NavLi>
-    <NavLi class='hover:text-[#1097ff]' href="/comunidad">Comunidad</NavLi>
+    <!-- <NavLi class='hover:text-[#1097ff]' href="/blog">Blog</NavLi>
+    <NavLi class='hover:text-[#1097ff]' href="/comunidad">Comunidad</NavLi> -->
   </NavUl>
 </Navbar>
 
