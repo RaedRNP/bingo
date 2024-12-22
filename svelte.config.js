fb250b7a0a973197264ff600509e5e9a57a0d0d5
RaedRNP <raedrnp10@gmail.com>
2024-12-21 21:29:29 -0400

    cambio de adapter a auto

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/svelte-adapter-github';
+import adapter from '@sveltejs/adapter-node';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
